bc2f09ceb9dec4e47e922a86788b1ce2ca1a1e0f
Mimi <cretu.mihaelaa11@gmail.com>
2023-03-10 16:24:26 +0200

    merge develop

--- src/customizations/components/theme/Footer/Footer.jsx
+++ src/customizations/components/theme/Footer/Footer.jsx
@@ -7,24 +7,44 @@ import React from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
 import { flattenToAppURL } from '@plone/volto/helpers';
 import EEAFooter from '@eeacms/volto-eea-design-system/ui/Footer/Footer';
+<<<<<<< HEAD
 import FooterSites from './FooterSites';
+=======
+>>>>>>> b3a64fe (merge develop)
 import config from '@plone/volto/registry';
 import { Grid } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
+<<<<<<< HEAD
 
 const Footer = () => {
   const { eea } = config.settings;
   const logo = eea.footerOpts.logoWhite;
+=======
+import LogoImage from '../Logo/Logo.png';
+
+const Footer = () => {
+  const { eea } = config.settings;
+>>>>>>> b3a64fe (merge develop)
   const {
     footerActions = [],
     copyrightActions = [],
     socialActions = [],
+<<<<<<< HEAD
+=======
+    contactActions = [],
+    contactExtraActions = [],
+>>>>>>> b3a64fe (merge develop)
   } = useSelector(
     (state) => ({
       footerActions: state.actions?.actions?.footer_actions,
       copyrightActions: state.actions?.actions?.copyright_actions,
       socialActions: state.actions?.actions?.social_actions,
+<<<<<<< HEAD
+=======
+      contactActions: state.actions?.actions?.contact_actions,
+      contactExtraActions: state.actions?.actions?.contact_extra_actions,
+>>>>>>> b3a64fe (merge develop)
     }),
     shallowEqual,
   );
@@ -54,9 +74,34 @@ const Footer = () => {
       }))
     : eea.footerOpts.social;
 
+<<<<<<< HEAD
   // Update options with actions from backend
   const options = {
     social,
+=======
+  // ZMI > portal_actions > contact_actions
+  const contacts = contactActions.length
+    ? contactActions.map((action, idx) => ({
+        text: action.title,
+        icon: action.icon,
+        link: flattenToAppURL(action.url),
+        children:
+          idx === 0
+            ? contactExtraActions.map((child) => ({
+                text: child.title,
+                icon: child.icon,
+                link: flattenToAppURL(child.url),
+              }))
+            : [],
+      }))
+    : eea.footerOpts.contacts;
+
+  // Update options with actions from backend
+  const options = {
+    // ...eea.footerOpts,
+    social,
+    // contacts,
+>>>>>>> b3a64fe (merge develop)
   };
 
   return (
@@ -98,7 +143,11 @@ const Footer = () => {
           <Grid.Column mobile={16} tablet={2} computer={2}>
             <img
               className="ab-footer"
+<<<<<<< HEAD
               src={logo}
+=======
+              src={LogoImage}
+>>>>>>> b3a64fe (merge develop)
               alt="ADVISORY"
               height={80}
               width={150}
@@ -115,23 +164,40 @@ const Footer = () => {
         <div className="subfooter-contact">
           <h5>Contact us</h5>
           <div className="subfooter-contact-info">
+<<<<<<< HEAD
             <a href="/contact">
               <i aria-hidden="true" className="icon mail outline"></i>
               Send us an e-mail
             </a>
             <a href="/contact">
               <i aria-hidden="true" className="icon call"></i>
+=======
+            <a href="/">
+              <i aria-hidden="true" class="icon mail outline"></i>
+              Send us an e-mail
+            </a>
+            <a href="/">
+              <i aria-hidden="true" class="icon call"></i>
+>>>>>>> b3a64fe (merge develop)
               Call us
             </a>
           </div>
         </div>
         <div className="subfooter-other">
           <h5>Other European Information Systems</h5>
+<<<<<<< HEAD
           {/* <EEAFooter.Sites sites={eea.footerOpts.sites} /> */}
           <FooterSites sites={eea.footerOpts.sites} />
         </div>
       </div>
 
+=======
+          <EEAFooter.Sites sites={eea.footerOpts.sites} />
+        </div>
+      </div>
+
+      {/* <EEAFooter.Header>{eea.footerOpts.header}</EEAFooter.Header> */}
+>>>>>>> b3a64fe (merge develop)
       <EEAFooter.Actions actions={actions} copyright={copyright} />
     </EEAFooter>
   );
