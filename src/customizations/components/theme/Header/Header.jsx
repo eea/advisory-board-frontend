eb840c20e19ed70c7bdaf2f2ad878e4817861cb6
ana-oprea <anaop3212@gmail.com>
2023-03-13 12:10:04 +0200

    feat: implement custom header

--- src/customizations/components/theme/Header/Header.jsx
+++ src/customizations/components/theme/Header/Header.jsx
@@ -2,34 +2,38 @@
  * Header component.
  * @module components/theme/Header/Header
  */
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Dropdown, Image, Container, Grid, Menu } from 'semantic-ui-react';
 import { connect, useDispatch, useSelector } from 'react-redux';
 
 import { withRouter } from 'react-router-dom';
-import { UniversalLink } from '@plone/volto/components';
+import { UniversalLink, SearchWidget } from '@plone/volto/components';
 import {
   getBaseUrl,
   hasApiExpander,
   flattenToAppURL,
 } from '@plone/volto/helpers';
 import { getNavigation } from '@plone/volto/actions';
-import { Logo } from '@eeacms/volto-eea-design-system/ui';
+import { Header, Logo } from '@eeacms/volto-eea-design-system/ui';
 import { usePrevious } from '@eeacms/volto-eea-design-system/helpers';
 import { find } from 'lodash';
+import HeaderMenuPopUp from './HeaderMenuPopUp';
+
+import closeIcon from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Header/close-line.svg';
+import burgerIcon from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Header/menu-line.svg';
 import globeIcon from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Header/global-line.svg';
+import eeaFlag from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Header/eea.png';
 
 import config from '@plone/volto/registry';
 import { compose } from 'recompose';
 
-import searchIcon from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Header/search-line.svg';
+import cx from 'classnames';
 
 function removeTrailingSlash(path) {
   return path.replace(/\/+$/, '');
 }
 
-const Header = ({ pathname, token, items, history, subsite }) => {
+const EEAHeader = ({ pathname, token, items, history, subsite }) => {
   const currentLang = useSelector((state) => state.intl.locale);
   const translations = useSelector(
     (state) => state.content.data?.['@components']?.translations?.items,
@@ -40,6 +44,23 @@ const Header = ({ pathname, token, items, history, subsite }) => {
   });
 
   const isSubsite = subsite?.['@type'] === 'Subsite';
+  const { eea } = config.settings;
+  const headerOpts = eea.headerOpts || {};
+  const { logo, logoWhite } = headerOpts || {};
+  const width = useSelector((state) => state.screen?.width);
+  const dispatch = useDispatch();
+  const previousToken = usePrevious(token);
+  const [language, setLanguage] = useState(currentLang || eea.defaultLanguage);
+
+  const [activeItem, setActiveItem] = useState(pathname);
+  const [menuIsActive, setMenuIsActive] = useState(false);
+  const [searchIsActive, setSearchIsActive] = useState(false);
+  const [burger, setBurger] = useState('');
+  const searchInputRef = React.useRef(null);
+
+  const node = React.useRef();
+  const mobileMenuBurgerRef = React.useRef();
+  const desktopMenuRef = React.useRef();
 
   const isHomePageInverse = useSelector((state) => {
     const layout = state.content?.data?.layout;
@@ -53,17 +74,35 @@ const Header = ({ pathname, token, items, history, subsite }) => {
     );
   });
 
-  const { eea } = config.settings;
-  const headerOpts = eea.headerOpts || {};
-  const { logo, logoWhite } = headerOpts || {};
-  const width = useSelector((state) => state.screen?.width);
-  const dispatch = useDispatch();
-  const previousToken = usePrevious(token);
-  const [language, setLanguage] = React.useState(
-    currentLang || eea.defaultLanguage,
-  );
+  const menuOnClickOutside = () => {
+    // restore active element if nothing was selected from the menu dropdown
+    if (pathname !== activeItem) {
+      setActiveItem(pathname);
+    }
+    // close mobile navigation when clicking outside if we have value for nav
+    if (burger) {
+      setBurger('');
+    }
+    // always close the  menu
+    setMenuIsActive(false);
+  };
 
-  React.useEffect(() => {
+  const mobileBurgerOnClick = () => {
+    if (searchIsActive === true) {
+      setSearchIsActive(false);
+    }
+
+    if (burger === '') {
+      setBurger('open');
+      setMenuIsActive(true);
+    } else {
+      setBurger('');
+      setMenuIsActive(false);
+      setActiveItem('');
+    }
+  };
+
+  useEffect(() => {
     const { settings } = config;
     const base_url = getBaseUrl(pathname);
     if (!hasApiExpander('navigation', base_url)) {
@@ -71,7 +110,7 @@ const Header = ({ pathname, token, items, history, subsite }) => {
     }
   }, [pathname, dispatch]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (token !== previousToken) {
       const { settings } = config;
       const base = getBaseUrl(pathname);
@@ -81,22 +120,21 @@ const Header = ({ pathname, token, items, history, subsite }) => {
     }
   }, [token, dispatch, pathname, previousToken]);
 
-  const [activeItem, setActiveItem] = React.useState(pathname);
-  const [menuIsActive, setMenuIsActive] = React.useState(false);
-  const [searchIsActive, setSearchIsActive] = React.useState(false);
-  const [burger, setBurger] = React.useState('');
-  const searchInputRef = React.useRef(null);
-
-  const node = React.useRef();
-  const searchButtonRef = React.useRef();
-  const mobileMenuBurgerRef = React.useRef();
-  const desktopMenuRef = React.useRef();
-
-  React.useEffect(() => {
+  useEffect(() => {
     if (searchIsActive) {
       searchInputRef.current && searchInputRef.current.focus();
     }
   }, [searchIsActive]);
+
+  useEffect(() => {
+    setMenuIsActive(false);
+    setSearchIsActive(false);
+    setBurger('');
+    // remove active menu when we have no pathname which means we hit logo to go home
+    if (!pathname) {
+      setActiveItem('');
+    }
+  }, [pathname]);
 
   const renderGlobalMenuItem = (item, { onClick }) => (
     <a
@@ -121,21 +159,107 @@ const Header = ({ pathname, token, items, history, subsite }) => {
     }
   };
 
-  const searchOnClick = (e, x) => {
-    if (menuIsActive === true) {
-      setBurger('');
-      setMenuIsActive(false);
-      setActiveItem('');
-    }
-    setSearchIsActive(!searchIsActive);
-  };
+  const [yOffset, setYOffset] = useState(
+    typeof window !== 'undefined' ? window.pageYOffset : '',
+  );
+  const [visible, setVisible] = useState(true);
+  const [topPage, setTopPage] = useState(true);
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  });
 
+  function handleScroll() {
+    const currentYOffset = window.pageYOffset;
+    const visible = yOffset > currentYOffset;
+    const isTopPage = currentYOffset < 1;
+    setYOffset(currentYOffset);
+    setVisible(visible);
+    setTopPage(isTopPage);
+  }
   return (
-    <div className="eea header">
+    <div
+      className={`eea header ${
+        visible
+          ? topPage
+            ? 'navbar navbar--static'
+            : 'navbar navbar--fixed'
+          : 'navbar--hidden'
+      }`}
+    >
+      <div className="top bar">
+        <Header.TopHeader>
+          <Header.TopItem className="official-union">
+            <Image src={eeaFlag} alt="eea flag"></Image>
+            <Header.TopDropdownMenu
+              text="An official website of the European Union | How do you know?"
+              tabletText="EEA information systems"
+              mobileText=" "
+              icon="chevron down"
+              aria-label="dropdown"
+              className=""
+              viewportWidth={width}
+            >
+              <div
+                className="content"
+                role="menu"
+                tabIndex="0"
+                onClick={(evt) => evt.stopPropagation()}
+                onKeyDown={(evt) => evt.stopPropagation()}
+              >
+                <p>
+                  All official European Union website addresses are in the{' '}
+                  <b>europa.eu</b> domain.
+                </p>
+                <a
+                  href="https://europa.eu/european-union/contact/institutions-bodies_en"
+                  target="_blank"
+                  rel="noreferrer"
+                  role="option"
+                  aria-selected="false"
+                >
+                  See all EU institutions and bodies
+                </a>
+              </div>
+            </Header.TopDropdownMenu>
+          </Header.TopItem>
+
+          {!!headerOpts.partnerLinks && (
+            <Header.TopItem>
+              <Header.TopDropdownMenu
+                id="theme-sites"
+                text={headerOpts.partnerLinks.title}
+                viewportWidth={width}
+              >
+                <div className="wrapper">
+                  {headerOpts.partnerLinks.links.map((item, index) => (
+                    <Dropdown.Item key={index}>
+                      <a
+                        href={item.href}
+                        className="site"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {item.title}
+                      </a>
+                    </Dropdown.Item>
+                  ))}
+                </div>
+              </Header.TopDropdownMenu>
+            </Header.TopItem>
+          )}
+        </Header.TopHeader>
+      </div>
       <div className={'main bar'} ref={node}>
         <Container>
           <Grid>
-            <Grid.Column mobile={8} tablet={8} computer={2}>
+            <Grid.Column
+              mobile={4}
+              tablet={4}
+              computer={2}
+              className="left-menu"
+              floated="left"
+            >
               <div {...(isSubsite ? { className: 'logo-wrapper' } : {})}>
                 <Logo
                   src={isHomePageInverse ? logoWhite : logo}
@@ -151,7 +275,12 @@ const Header = ({ pathname, token, items, history, subsite }) => {
                 )}
               </div>
             </Grid.Column>
-            <Grid.Column mobile={4} tablet={4} computer={6}>
+            <Grid.Column
+              mobile={1}
+              tablet={2}
+              computer={7}
+              className="mobile hidden tablet hidden"
+            >
               <div
                 className={
                   isHomePageInverse ? 'main-menu inverted' : 'main-menu'
@@ -182,12 +311,13 @@ const Header = ({ pathname, token, items, history, subsite }) => {
               </div>
             </Grid.Column>
             <Grid.Column
-              mobile={8}
-              tablet={8}
+              mobile={6}
+              tablet={6}
               computer={3}
               className="right-menu"
+              floated="right"
             >
-              <div className="language-column">
+              <div className="language-column mobile hidden tablet hidden">
                 <Dropdown
                   id={'language-switcher'}
                   className={'item header-top-item'}
@@ -237,18 +367,44 @@ const Header = ({ pathname, token, items, history, subsite }) => {
                 </Dropdown>
               </div>
               <div className="search-ab">
-                <div
-                  className="search-icon-ab"
-                  onClick={searchOnClick}
-                  role="none"
-                  ref={searchButtonRef}
-                >
-                  <Image src={searchIcon} alt="search button open/close" />
-                </div>
-                <input className="search-input-ab" placeholder="Search" />
+                <SearchWidget pathname={pathname} />
               </div>
+              <Header.BurgerAction
+                className={`mobile ${burger} ${
+                  visible ? 'burger-transition-in' : 'burger-transition-out'
+                }`}
+                onClick={mobileBurgerOnClick}
+                ref={mobileMenuBurgerRef}
+              >
+                <Image
+                  src={burger === 'open' ? `${closeIcon}` : `${burgerIcon}`}
+                  alt="menu icon open/close"
+                />
+              </Header.BurgerAction>
             </Grid.Column>
           </Grid>
+          <HeaderMenuPopUp
+            renderMenuItem={(item, options, props) => (
+              <UniversalLink
+                href={item.url || '/'}
+                title={item.nav_title || item.title}
+                {...(options || {})}
+                className={cx(options?.className, {
+                  active: item.url === router_pathname,
+                })}
+              >
+                {props?.iconPosition !== 'right' && props?.children}
+                <span>{item.nav_title || item.title}</span>
+                {props?.iconPosition === 'right' && props?.children}
+              </UniversalLink>
+            )}
+            activeItem={activeItem}
+            menuItems={items}
+            pathName={pathname}
+            onClose={menuOnClickOutside}
+            triggerRefs={[mobileMenuBurgerRef, desktopMenuRef]}
+            visible={menuIsActive}
+          />
         </Container>
       </div>
     </div>
@@ -265,4 +421,4 @@ export default compose(
     }),
     { getNavigation },
   ),
-)(Header);
+)(EEAHeader);
