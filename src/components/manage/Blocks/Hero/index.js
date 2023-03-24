65cb8965384a6ad32dc79837dd7cf5dabcd503df
Miu Razvan <miu.razvan28@gmail.com>
2023-03-24 18:56:28 +0200

    customize hero block schema, waiting for eea-kitkat@13.1.0 to be released

--- src/components/manage/Blocks/Hero/index.js
+++ src/components/manage/Blocks/Hero/index.js
@@ -1,10 +1,12 @@
 import HeroEdit from './Edit';
 import HeroView from './View';
+import schema from './schema';
 
 export default (config) => {
   if (config.blocks.blocksConfig.hero) {
     config.blocks.blocksConfig.hero = {
       ...config.blocks.blocksConfig.hero,
+      schema,
       edit: HeroEdit,
       view: HeroView,
     };
