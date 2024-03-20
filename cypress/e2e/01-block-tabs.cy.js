6f6ee1ea3a022afb05c62467f553fea1e95e5cfe
Teodor <teodorvoicu87@gmail.com>
2024-03-20 19:09:02 +0200

    Refs #267222 - fix integration tests for tabs-block

--- cypress/e2e/01-block-tabs.cy.js
+++ cypress/e2e/01-block-tabs.cy.js
@@ -4,30 +4,46 @@ describe('Blocks Tests', () => {
   beforeEach(slateBeforeEach);
   afterEach(slateAfterEach);
 
-  it('Add Block: Empty', () => {
-    // Change page title
+  it('Check tabs widget', () => {
     cy.clearSlateTitle();
-    cy.getSlateTitle().type('My Add-on Page');
+    cy.getSlateTitle().type('Tabs widget');
 
-    cy.get('.documentFirstHeading').contains('My Add-on Page');
-
-    cy.getSlate().click({force: true});
-
-    // Add block
-    cy.get('.ui.basic.icon.button.block-add-button').first().click({force: true});
-    cy.get('.blocks-chooser .title').contains('Media').click();
-    cy.get('.content.active.media .button.image').contains('Image').click();
+    cy.get('.documentFirstHeading').contains('Tabs widget');
 
     cy.getSlate().click();
 
-    cy.get('.ui.basic.icon.button.block-add-button').first().click({force: true});
+    cy.get('.ui.basic.icon.button.block-add-button').first().click();
     cy.get('.blocks-chooser .title').contains('Common').click();
     cy.get('.content.active.common .button.tabs_block')
       .contains('Tabs')
       .click({ force: true });
 
     cy.get('.field-wrapper-title input').last().type('Tab 1');
-    cy.get('.field-wrapper-template #field-template').click();
+    cy.get('.tabs-area .accordion.ui').first().click();
+    cy.get('#field-assetType-1-data-0').click();
+    cy.contains('Icon').click();
+    cy.get('#field-icon-2-data-0').type('delete');
+    cy.get('#toolbar-save').click();
+  });
+
+  it('Add Tabs default template', () => {
+    cy.clearSlateTitle();
+    cy.getSlateTitle().type('Tabs block default template');
+
+    cy.get('.documentFirstHeading').contains('Tabs block default template');
+
+    cy.getSlate().click({ force: true });
+
+    cy.get('.ui.basic.icon.button.block-add-button')
+      .first()
+      .click({ force: true });
+    cy.get('.blocks-chooser .title').contains('Common').click();
+    cy.get('.content.active.common .button.tabs_block')
+      .contains('Tabs')
+      .click({ force: true });
+
+    cy.get('.field-wrapper-title input').last().type('Tab 1');
+    cy.get('.field-wrapper-variation #field-variation').click();
     cy.get('.react-select__menu').contains('Default').click();
     cy.get('.field-wrapper-verticalAlign #field-verticalAlign').click();
     cy.get('.react-select__menu').contains('Middle').click();
@@ -37,40 +53,136 @@ describe('Blocks Tests', () => {
     cy.get('.field-wrapper-menuPosition #field-menuPosition').first().click();
     cy.get('.react-select__menu').contains('Top').click();
 
-    cy.scrollTo(0, 500)
+    cy.get('.field-wrapper-menuPosition #field-menuPosition').first().click();
+    cy.get('.react-select__menu').contains('Left').click();
+
+    cy.get('.field-wrapper-menuPosition #field-menuPosition').first().click();
+    cy.get('.react-select__menu').contains('Right').click();
+
+    cy.get('.field-wrapper-menuPosition #field-menuPosition').first().click();
+    cy.get('.react-select__menu').contains('Bottom').click();
+
+    cy.get('.field-wrapper-menuPosition #field-menuPosition').first().click();
+    cy.get('.react-select__menu').contains('Top').click();
+
     cy.get('.tabs-block [contenteditable=true]').first().type('Hydrogen');
     cy.get('.tabs-block .ui.left.menu .item').last().click();
-    cy.get('.tabs-block').contains('Tab 2').click();
     cy.get('.tabs-block.edit [contenteditable=true]').first().type('Oxygen');
+    cy.get('.tabs-block a.item').first().type(' ');
 
-    cy.getSlate().click();
-    cy.get('.ui.basic.icon.button.block-add-button').first().click({force: true});
+    cy.get('#toolbar-save').click();
+    cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
+
+    // then the page view should contain our changes
+    cy.contains('Tabs block default template');
+
+    cy.get('.tabs-block .menu-item-text')
+      .contains('Tab 2')
+      .parent()
+      .focus()
+      .type('{enter}');
+
+    cy.contains('Oxygen');
+
+    cy.get('.tabs-block .menu-item-text').contains('Tab 1').click();
+
+    cy.contains('Hydrogen');
+  });
+
+  it('Add Tabs carousel template', () => {
+    // Change page title
+    cy.clearSlateTitle();
+    cy.getSlateTitle().type('Tabs block carousel template');
+
+    cy.get('.documentFirstHeading').contains('Tabs block carousel template');
+
+    cy.getSlate().click({ force: true });
+
+    cy.get('.ui.basic.icon.button.block-add-button')
+      .first()
+      .click({ force: true });
     cy.get('.blocks-chooser .title').contains('Common').click();
     cy.get('.content.active.common .button.tabs_block')
       .contains('Tabs')
       .click({ force: true });
 
-    cy.get('.field-wrapper-template #field-template').click();
+    cy.get('.field-wrapper-variation #field-variation').click();
     cy.get('.react-select__menu').contains('Carousel horizontal').click();
     cy.get('.field-wrapper-verticalAlign #field-verticalAlign').click();
-    cy.get('.react-select__menu').contains('Bottom').click();
+    cy.get('.react-select__menu').contains('Top').click();
     cy.get('.field-wrapper-theme #field-theme').click();
     cy.get('.react-select__menu').contains('Dark').click();
+    cy.get('.tabs-block [contenteditable=true]').first().type('Hydrogen');
     cy.get('.tabs-block .ui.menu .item').last().click();
     cy.get('.tabs-block .ui.menu .item').eq(1).click();
+    cy.get('.tabs-block').contains('Tab 2').click();
+    cy.get('.tabs-block.edit [contenteditable=true]').first().type('Oxygen');
 
     // Save
     cy.get('#toolbar-save').click();
     cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
 
     // then the page view should contain our changes
-    cy.contains('My Add-on Page');
+    cy.contains('Tabs block carousel template');
+    cy.wait(500);
     cy.contains('Hydrogen');
 
-    cy.get('.tabs-block p').contains('Tab 2').click();
-    cy.contains('Oxygen');
-
     cy.get('.slick-arrow').click();
-    cy.get('.block.image');
+    cy.contains('Oxygen');
+  });
+
+  it('Add Tabs Block Horizontal', () => {
+    // Change page title
+    cy.clearSlateTitle();
+    cy.getSlateTitle().type('Tabs block horizontal template');
+
+    cy.get('.documentFirstHeading').contains('Tabs block horizontal template');
+    cy.getSlate().click({ force: true });
+
+    cy.get('.ui.basic.icon.button.block-add-button')
+      .first()
+      .click({ force: true });
+    cy.get('.blocks-chooser .title').contains('Common').click();
+    cy.get('.content.active.common .button.tabs_block')
+      .contains('Tabs')
+      .click({ force: true });
+
+    cy.get('.field-wrapper-title input').last().type('Tab 1');
+    cy.get('.field-wrapper-variation #field-variation').click();
+    cy.get('.react-select__menu').contains('Horizontal responsive').click();
+
+    cy.get('.tabs-block [contenteditable=true]')
+      .first()
+      .type('Horizontal First Item');
+    cy.get('.tabs-block .horizontal-responsive .ui.menu .item')
+      .last()
+      .click({ force: true });
+    cy.get('.tabs-block').contains('Tab 2').click();
+    cy.get('.tabs-block.edit [contenteditable=true]')
+      .first()
+      .type('Horizontal Second Item');
+    cy.get('.tabs-block .horizontal-responsive .ui.menu .item')
+      .last()
+      .click({ force: true });
+    cy.get('.tabs-block').contains('Tab 3').dblclick().type('Tab 3 edited');
+
+    // Save
+    cy.get('#toolbar-save').click();
+    cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
+
+    // then the page view should contain our changes
+    cy.contains('Tabs block horizontal template');
+    cy.get('.tabs-block .horizontal-responsive.tabs').should('exist');
+    cy.contains('Horizontal First Item');
+
+    cy.get('.tabs-block .menu-item-text')
+      .contains('Tab 2')
+      .parent()
+      .focus()
+      .type('{enter}');
+    cy.contains('Horizontal Second Item');
+
+    cy.get('.tabs-block .menu-item-text').contains('Tab 1').click();
+    cy.contains('Horizontal First Item');
   });
 });
